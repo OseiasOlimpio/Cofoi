4b0a887ad49a75e4704d66f63615573b1861ea28
Oseias Olimpio <oseiasolimpio96@gmail.com>
2024-09-23 11:55:52 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-const wss = new WebSocketServer({ port: process.env.PORT || 3000 });
+const wss = new WebSocketServer({ port: process.env.PORT || 5000 });
 
 wss.on("connection", ws => {
   ws.on("error", console.error);
