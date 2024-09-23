5e8aff85a9949289d389ca3338211daa6d4798eb
Oseias Olimpio <oseiasolimpio96@gmail.com>
2024-09-23 12:03:30 -0300

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,16 @@
-const { WebSocketServer } = require("ws"); //importa o websocket
+//Importar módulos
+const { WebSocketServer } = require("ws"); 
 const dotenv = require("dotenv");
 
+//Configuração dp servidor
 dotenv.config();
-
 const wss = new WebSocketServer({ port: process.env.PORT || 5000 });
 
+//Tratamento da conexão
 wss.on("connection", ws => {
   ws.on("error", console.error);
-
   ws.on("message", (data) => {
     wss.clients.forEach((client) => client.send(data.toString()));
   });
-
   console.log("client connected")
 });
